da8ee86260b3f44f07baf52eb5b740147dc06aad
jj484 <jj484@njit.edu>
2020-01-11 20:06:26 -0500

    made a video component

---
+++ src/components/video.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default class Video extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Video</h1>
+        <p>There will be a video below</p>
+      </div>
+    )
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Video from "../components/video"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <button>Click me!</button>
+      <Video />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
